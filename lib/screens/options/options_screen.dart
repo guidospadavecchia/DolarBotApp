8b8114d6fe2355d9eb2e9071ace9f39e1dfc5b51
Guido Spadavecchia <guido.spadavecchia@gmail.com>
2021-02-18 01:05:01 -0300

    Update options_screen.dart

--- lib/screens/options/options_screen.dart
+++ lib/screens/options/options_screen.dart
@@ -1,7 +1,7 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:dolarbot_app/classes/theme_manager.dart';
 import 'package:dolarbot_app/screens/options/widgets/format_currency_dialog/format_currency_dialog.dart';
-import 'package:dolarbot_app/widgets/common/appBar.dart';
+import 'package:dolarbot_app/widgets/common/common_app_bar.dart';
 import 'package:dolarbot_app/widgets/common/menu_item.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
