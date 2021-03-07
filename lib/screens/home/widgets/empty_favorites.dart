ffbc9d0a6f27ea12875964b2f1d4a8fb52ebd746
Guido Spadavecchia <guido.spadavecchia@gmail.com>
2021-03-07 16:49:19 -0300

    Update empty_favorites.dart

--- lib/screens/home/widgets/empty_favorites.dart
+++ lib/screens/home/widgets/empty_favorites.dart
@@ -1,24 +1,28 @@
 import 'package:dolarbot_app/classes/theme_manager.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class EmptyFavorites extends StatelessWidget {
+  final String topText;
+  final String bottomText;
+
   const EmptyFavorites({
     Key key,
+    this.topText,
+    this.bottomText,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Opacity(
-        opacity: 1,
+        opacity: 0.8,
         child: Container(
           alignment: Alignment.topCenter,
           width: MediaQuery.of(context).size.width * 0.8,
           child: Column(
             children: [
               Text(
-                "Acá aparecerán tus cotizaciones favoritas.",
+                topText ?? "¡Aquí aparecerán tus cotizaciones favoritas!",
                 style: TextStyle(
                     fontSize: 24,
                     fontFamily: "Raleway",
@@ -26,16 +30,16 @@ class EmptyFavorites extends StatelessWidget {
                         .withOpacity(0.7)),
                 textAlign: TextAlign.center,
               ),
-              SizedBox(height: 30),
-              Icon(
-                FontAwesomeIcons.solidHandPointDown,
-                size: 48,
-                color: ThemeManager.getPrimaryAccentColor(context)
-                    .withOpacity(0.8),
-              ),
-              SizedBox(height: 30),
+              // SizedBox(height: 30),
+              // Icon(
+              //   FontAwesomeIcons.solidHandPointDown,
+              //   size: 48,
+              //   color: ThemeManager.getPrimaryAccentColor(context)
+              //       .withOpacity(0.8),
+              // ),
+              SizedBox(height: 50),
               Opacity(
-                opacity: 0.7,
+                opacity: 0.4,
                 child: SizedBox(
                   width: MediaQuery.of(context).size.width * 0.7,
                   child: Image.asset("assets/images/general/home_bg.png"),
@@ -43,7 +47,8 @@ class EmptyFavorites extends StatelessWidget {
               ),
               SizedBox(height: 50),
               Text(
-                "Ahora mismo no tenés ninguna, pero podés dirigirte a cualquier cotización y agregarla desde allí.",
+                bottomText ??
+                    "Ahora mismo no tenés ninguna. Podés dirigirte a cualquier cotización y agregarla desde allí",
                 style: TextStyle(
                     fontSize: 16,
                     fontFamily: "Raleway",
