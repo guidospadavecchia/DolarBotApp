f8f13d20d1f8bc51d1b99632eee284cc0d2510df
Juan Manuel Flecha <flechajm@gmail.com>
2021-03-16 23:54:15 -0300

    Agregado de parámetros en ErrorScreen

--- lib/screens/common/error_screen.dart
+++ lib/screens/common/error_screen.dart
@@ -2,8 +2,13 @@ import 'package:dolarbot_app/classes/theme_manager.dart';
 import 'package:flutter/material.dart';
 
 class ErrorScreen extends StatelessWidget {
+  final Color color;
+  final double opacity;
+
   const ErrorScreen({
     Key key,
+    this.color,
+    this.opacity = 0.6,
   }) : super(key: key);
 
   @override
@@ -17,9 +22,10 @@ class ErrorScreen extends StatelessWidget {
           child: Text(
             "Ouch! Ocurrió un error\nal obtener la cotización",
             style: TextStyle(
-                fontSize: 24,
-                fontFamily: "Raleway",
-                color: ThemeManager.getPrimaryTextColor(context)),
+              fontSize: 24,
+              fontFamily: "Raleway",
+              color: color,
+            ),
             textAlign: TextAlign.center,
           ),
         ),
@@ -27,14 +33,16 @@ class ErrorScreen extends StatelessWidget {
           height: 30,
         ),
         Opacity(
-          opacity: 0.5,
+          opacity: opacity,
           child: Container(
-            height: MediaQuery.of(context).size.height / 6,
+            height: MediaQuery.of(context).size.height / 5,
             decoration: BoxDecoration(
               image: new DecorationImage(
                 fit: BoxFit.scaleDown,
                 colorFilter: ColorFilter.mode(
-                    ThemeManager.getGlobalBackgroundColor(context).withOpacity(0.8),
+                    color == null
+                        ? ThemeManager.getForegroundColor().withOpacity(opacity)
+                        : color.withOpacity(opacity),
                     BlendMode.srcATop),
                 image: AssetImage("assets/images/general/error.png"),
               ),
