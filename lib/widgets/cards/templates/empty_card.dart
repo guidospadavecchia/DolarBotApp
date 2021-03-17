0f39fbaceee9af413d876e48dd79af7697d61cdc
Juan Manuel Flecha <flechajm@gmail.com>
2021-03-16 23:24:26 -0300

    EmptyCard

--- lib/widgets/cards/templates/empty_card.dart
+++ lib/widgets/cards/templates/empty_card.dart
@@ -1,9 +1,5 @@
-import 'package:dolarbot_app/util/constants.dart';
 import 'package:dolarbot_app/widgets/cards/templates/base/base_card.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-import '../card_favorite.dart';
 
 class EmptyCard extends BaseCard {
   @override
@@ -13,23 +9,6 @@ class EmptyCard extends BaseCard {
 class _EmptyCardState extends BaseCardState<EmptyCard> {
   @override
   Widget card() {
-    return Opacity(
-      opacity: 0.4,
-      child: CardFavorite(
-        height: 65,
-        header: CardHeader(
-          title: 'Error al obtener cotización',
-          shareButton: null,
-        ),
-        direction: Axis.vertical,
-        rates: [],
-        logo: CardLogo(
-          iconData: FontAwesomeIcons.times,
-          tag: null,
-        ),
-        gradiantColors: DolarBotConstants.kGradiantError,
-        lastUpdated: null,
-      ),
-    );
+    return SizedBox.shrink();
   }
 }
