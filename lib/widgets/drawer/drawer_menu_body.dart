0e95142bdcb89bef9ab67d598531c3c5545ac198
Guido Spadavecchia <guido.spadavecchia@gmail.com>
2021-05-06 00:14:56 -0300

    Agregado de scroll-to-top button al DrawerBody

--- lib/widgets/drawer/drawer_menu_body.dart
+++ lib/widgets/drawer/drawer_menu_body.dart
@@ -1,11 +1,14 @@
 import 'package:dolarbot_app/classes/theme_manager.dart';
 import 'package:dolarbot_app/screens/base/base_info_screen.dart';
 import 'package:dolarbot_app/widgets/drawer/root_menus/exports/root_menu_exports.dart';
-
 import 'package:flutter/material.dart';
+import 'package:flutter_scroll_to_top/flutter_scroll_to_top.dart';
+
 export 'package:dolarbot_app/util/util.dart';
 
 class DrawerMenuBody extends StatelessWidget {
+  final ScrollController _scrollController = ScrollController();
+
   @override
   Widget build(BuildContext context) {
     return Expanded(
@@ -14,17 +17,28 @@ class DrawerMenuBody extends StatelessWidget {
           overScroll.disallowGlow();
           return false;
         },
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            RootMenuHome(),
-            RootMenuDollar(),
-            RootMenuBanks(),
-            RootMenuCrypto(),
-            RootMenuMetals(),
-            RootMenuBCRA(),
-            RootMenuVenezuela()
-          ],
+        child: ScrollWrapper(
+          scrollController: _scrollController,
+          promptAlignment: Alignment.topCenter,
+          promptDuration: const Duration(milliseconds: 500),
+          promptTheme: PromptButtonTheme(
+            color: ThemeManager.getDarkThemeData().backgroundColor.withOpacity(0.9),
+            padding: const EdgeInsets.only(top: 15),
+            iconPadding: const EdgeInsets.all(10),
+          ),
+          child: ListView(
+            controller: _scrollController,
+            padding: EdgeInsets.zero,
+            children: [
+              RootMenuHome(),
+              RootMenuDollar(),
+              RootMenuBanks(),
+              RootMenuCrypto(),
+              RootMenuMetals(),
+              RootMenuBCRA(),
+              RootMenuVenezuela()
+            ],
+          ),
         ),
       ),
     );
