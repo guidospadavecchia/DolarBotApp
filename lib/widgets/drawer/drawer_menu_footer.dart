76c34f2041da78dbbbc7d685f5e54166ca66e9b5
Juan Manuel Flecha <flechajm@gmail.com>
2021-05-07 18:13:00 -0300

    Cambio de footer DolarBot Pro

--- lib/widgets/drawer/drawer_menu_footer.dart
+++ lib/widgets/drawer/drawer_menu_footer.dart
@@ -3,6 +3,7 @@ import 'package:dolarbot_app/classes/app_config.dart';
 import 'package:dolarbot_app/classes/theme_manager.dart';
 import 'package:dolarbot_app/screens/options/options_screen.dart';
 import 'package:dolarbot_app/widgets/common/menu_item.dart';
+import 'package:dolarbot_app/widgets/common/pills/pro_pill.dart';
 import 'package:dolarbot_app/widgets/common/pro_features/pro_features_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -23,11 +24,13 @@ class DrawerMenuFooter extends StatelessWidget {
                 children: [
                   MenuItem(
                     text: "DolarBot Pro",
-                    textColor: Colors.deepOrange,
-                    leading: Icon(
-                      FontAwesomeIcons.googlePlay,
-                      color: Colors.deepOrange,
+                    leading: Image.asset(
+                      "assets/images/general/googleplay.png",
+                      width: 24,
+                      height: 24,
+                      filterQuality: FilterQuality.high,
                     ),
+                    trailing: ProPill(),
                     depthLevel: 1,
                     onTap: () => showDialog(context: context, builder: (_) => ProFeaturesDialog()),
                   ),
